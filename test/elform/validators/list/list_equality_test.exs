9c4a4c9d5d1bbf852245bc7175c71200b7d5188f
filipewelton1 <filipewelton1@gmail.com>
2024-01-15 20:31:04 -0300

    Add list equality validation test

---
+++ test/elform/validators/list/list_equality_test.exs
@@ -0,0 +1,37 @@
+defmodule Elform.Validators.List.ListEqualityTest do
+  use ExUnit.Case
+  use Elform
+
+  describe "call/2" do
+    @list Faker.Lorem.words()
+
+    test "when the list is different from that required" do
+      schema = %{
+        label: "words",
+        validators: [list_equality: @list]
+      }
+
+      payload = %{"words" => Faker.Lorem.words()}
+      response = validate(schema, payload)
+      list = Enum.sort(@list) |> Enum.join(",")
+
+      expected_response =
+        %{"words" => [list_equality: "the list must be equal to [#{list}]"]}
+
+      assert response == expected_response
+    end
+
+    test "when the list is equal to the required" do
+      schema = %{
+        label: "words",
+        validators: [list_equality: @list]
+      }
+
+      payload = %{"words" => @list}
+      response = validate(schema, payload)
+      expected_response = %{"words" => :ok}
+
+      assert response == expected_response
+    end
+  end
+end
