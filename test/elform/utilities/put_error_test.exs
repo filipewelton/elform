320ab4b646cc52e526836fd16b3870d8ff0c3acd
filipewelton1 <filipewelton1@gmail.com>
2024-01-18 18:36:38 -0300

    Add utilities tests

--- test/elform/utilities/put_error_test.exs
+++ test/elform/utilities/put_error_test.exs
@@ -4,18 +4,17 @@ defmodule Elform.Utilities.PutErrorTest do
   alias Elform.Schema
   alias Elform.Utilities.PutError
 
-  describe "call/2" do
-    test "when all arguments is valid" do
-      schema = %Schema{
-        errors: [],
-        label: "something",
-        required: true,
-        valid?: true,
-        validators: [],
-      }
-      %Schema{errors: errors} = PutError.call(schema, error: "unknown")
-      expected_error = [error: "unknown"]
-      assert errors == expected_error
+  describe "when all arguments is valid" do
+    test "should return unknown error" do
+      %Schema{errors: errors} = Schema.cast(%{
+        label: "email",
+        validators: []
+      })
+      |> PutError.call(error: "unknown")
+
+      expected_errors = [error: "unknown"]
+
+      assert errors == expected_errors
     end
   end
 end
