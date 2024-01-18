2172b39637d5e2cb6eeaa50fd89cf29a39f60eef
filipewelton1 <filipewelton1@gmail.com>
2024-01-18 10:09:57 -0300

    Add utilities modules

--- lib/elform/utilities/key_list.ex
+++ lib/elform/utilities/key_list.ex
@@ -1,13 +1,28 @@
 defmodule Elform.Utilities.KeyList do
-  def valid?(list) when list === [], do: true
+  def check_keys!(list) do
+    if key_list?(list) do
+      validators = Application.fetch_env!(:elform, :validators)
+      k = keys(validators)
+      l = drop(list, k)
 
-  def valid?(list) when is_list(list) do
+      case l === [] do
+        true -> :ok
+        false -> raise ArgumentError, "Argument has an invalid key(s)"
+      end
+    else
+      raise ArgumentError, "Argument should be a keyword or atom list"
+    end
+  end
+
+  defp key_list?(list) when list === [], do: true
+
+  defp key_list?(list) when is_list(list) do
     not Enum.any?(list, fn e ->
       not is_atom(e) and not is_tuple(e)
     end)
   end
 
-  def valid?(_), do: false
+  defp key_list?(_), do: false
 
   def keys(list) when list == [], do: []
 
@@ -28,4 +43,25 @@ defmodule Elform.Utilities.KeyList do
     keys(list)
     |> Enum.filter(&(&1 not in keys))
   end
+
+  def fetch!(list, _key) when list == [], do: raise(KeyError, "Not found key")
+
+  def fetch!(list, key) do
+    result =
+      Enum.filter(list, fn e ->
+        cond do
+          is_atom(e) ->
+            e === key
+
+          is_tuple(e) ->
+            {k, _v} = e
+            k === key
+        end
+      end)
+      |> List.first()
+
+    if result == nil, do: raise(KeyError, "Not found key"), else: result
+  end
+
+  def merge(list1, list2), do: list1 ++ list2
 end
--- lib/elform/utilities/validator_action_fallback.ex
+++ lib/elform/utilities/validator_action_fallback.ex
@@ -1,4 +1,4 @@
-defmodule Elform.Utilities.ActionFallback do
+defmodule Elform.Utilities.ValidatorActionFallback do
   defmacro __using__(opts) do
     name = opts[:name]
     type = opts[:type]
@@ -6,7 +6,7 @@ defmodule Elform.Utilities.ActionFallback do
     if name == nil or type == nil do
       quote do
         def call(_, _) do
-          {:error, "The action fallback must receive name and type arguments"}
+          {:error, "The action fallback should receive name and type arguments"}
         end
       end
     else
@@ -18,31 +18,31 @@ defmodule Elform.Utilities.ActionFallback do
         cond do
           unquote(type) == :string ->
             def call(value, _arg) when not is_bitstring(value) do
-              [{unquote(name), "the value must be a string"}]
+              [{unquote(name), "the value should be a string"}]
             end
 
           unquote(type) == :number ->
             def call(value, _arg) when not is_number(value) do
-              [{unquote(name), "the value must be a number"}]
+              [{unquote(name), "the value should be a number"}]
             end
 
             def call(value) when not is_number(value) do
-              [{unquote(name), "the value must be a number"}]
+              [{unquote(name), "the value should be a number"}]
             end
 
           unquote(type) == :boolean ->
             def call(value, _arg) when not is_boolean(value) do
-              [{unquote(name), "the value must be a boolean"}]
+              [{unquote(name), "the value should be a boolean"}]
             end
 
           unquote(type) == :list ->
             def call(value, _arg) when not is_list(value) do
-              [{unquote(name), "the value must be a list"}]
+              [{unquote(name), "the value should be a list"}]
             end
 
           unquote(type) == :map ->
             def call(value, _arg) when not is_map(value) do
-              [{unquote(name), "the value must be a map"}]
+              [{unquote(name), "the value should be a map"}]
             end
 
           true ->
