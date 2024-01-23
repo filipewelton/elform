d28e16f30ec8acfc074f638cac44d1ed8b89d22b
filipewelton1 <filipewelton1@gmail.com>
2024-01-23 19:46:46 -0300

    Restructure the project

--- lib/elform/validators/behaviour.ex
+++
@@ -1,7 +0,0 @@
-defmodule Elform.Validators.Behaviour do
-  @typep value :: map() | number() | boolean() | list() | String.t()
-  @typep args :: number() | tuple() | map() | list() | Regex.t() | String.t()
-  @typep reply :: :ok | keyword()
-
-  @callback call(value(), args()) :: reply()
-end
